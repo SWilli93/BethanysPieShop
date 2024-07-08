960a30d1bf0e3d3ae74fec440eea50522b64ef76
Scott <70402467+SWilli93@users.noreply.github.com>
2024-07-08 15:26:30 -0400

    Contact Page, Email Tag helper

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult index()
+    {
+        return View();
+    }
+}
